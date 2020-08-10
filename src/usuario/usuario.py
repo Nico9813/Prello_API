cda8367e79b27e890665a2633d9a4dad6a4f7fd8
nico9813 <nico.gomez.mbc@gmail.com>
2020-08-10 20:03:23 -0300

    reestructuro carpetas db

--- src/usuario/usuario.py
+++ src/usuario/usuario.py
@@ -1,10 +1,17 @@
+from tablero import Tablero
+
 class Usuario:
     nombre : str
     edad : int
+    tableros : list(Tablero)
 
     def __init__(self, nombre : str, edad : int):
         self.nombre = nombre
         self.edad = edad
+        self.tableros = []
     
     def __str__(self):
-        return "Nombre: " + self.nombre + ", Edad: " + self.edad
+        return "Nombre: " + self.nombre + ", Edad: " + self.edad + ", Tableros: " + len(self.tableros)
+
+    def agregarTablero(nombre : str):
+        self.tableros.append(Tablero(nombre))
